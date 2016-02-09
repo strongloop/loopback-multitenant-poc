8d65cbae9ae1aab63554eacff1331886ca49f446
Hage Yaapa <captain@hacksparrow.com>
2016-02-09 18:00:42 +0530

    add model

--- manager.js
+++ manager.js
@@ -14,7 +14,6 @@ if (argv._.length < 2) {
   return console.log('\n Usage: mt %s <tenant id>\n', command)
 }
 
-var tenantId = argv._[1].toLowerCase();
 var tenantDir = 'tenants/' + tenantId;
 var tenantProcesses = {};
 
@@ -25,33 +24,38 @@ switch (command) {
 
   // add tenant
   case 'add':
+    var tenantId = argv._[1].toLowerCase();
     commandLib.add(tenantId)
     break;
 
   // delete tenant
   case 'remove':
   case 'delete':
+    var tenantId = argv._[1].toLowerCase();
     commandLib.remove(tenantId)
     break;
 
   // start tenant app
   case 'start':
+    var tenantId = argv._[1].toLowerCase();
     commandLib.start(tenantId)
     break;
 
   // restart tenant app
   case 'restart':
+    var tenantId = argv._[1].toLowerCase();
     commandLib.restart(tenantId)
     break;
 
   // stop tenant app
   case 'stop':
+    var tenantId = argv._[1].toLowerCase();
     commandLib.stop(tenantId)
     break;
 
   // stop tenant app
   case 'model':
-    commandLib.model(tenantId)
+    commandLib.model(argv._[1]);
     break;
 
   default:
